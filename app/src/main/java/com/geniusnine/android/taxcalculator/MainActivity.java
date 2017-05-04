900b2d5b284a32cd73546fd45376a2aa42d9d3c4
geniusninedev <developer@geniusnine.com>
2017-05-04 11:47:20 +0530

    new thread created

--- app/src/main/java/com/geniusnine/android/taxcalculator/MainActivity.java
+++ app/src/main/java/com/geniusnine/android/taxcalculator/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -55,7 +56,7 @@ public class MainActivity extends AppCompatActivity
     private static final int PERMISSION_REQUEST_CODE = 200;
     private FragmentPagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
-
+    Handler handler;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListner;
     private DatabaseReference databaseReferenceUserContacts;
@@ -117,7 +118,19 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ForumActivity.class));
+                handler = new Handler();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                startActivity(new Intent(MainActivity.this, ForumActivity.class));
+                            }
+                        });
+
+                    }
+                }).start();
             }
         });
 
